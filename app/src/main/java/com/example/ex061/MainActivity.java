a405f1307bbdcd75d2f50e5f62929da2e114c551
orira22 <oriroitzaid@gmail.com>
2023-04-03 18:31:04 +0300

    finished the design and the java code - the background color is changed according to the state of the buttons

--- app/src/main/java/com/example/ex061/MainActivity.java
+++ app/src/main/java/com/example/ex061/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.ex061;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ToggleButton toggle;
+    Switch switch1;
+    View mainView;
+
+    boolean toggleChecked;
+    boolean switchChecked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toggle = findViewById(R.id.toggle);
+        switch1 = findViewById(R.id.switch1);
+        mainView = findViewById(R.id.view);
+    }
+
+    public void changeBackground(View view) {
+        toggleChecked = toggle.isChecked();
+        switchChecked = switch1.isChecked();
+
+        if(toggleChecked && switchChecked) {
+            mainView.setBackgroundColor(Color.RED);
+        }
+        else if(toggleChecked && !switchChecked) {
+            mainView.setBackgroundColor(Color.CYAN);
+        }
+        else if(!toggleChecked && switchChecked) {
+            mainView.setBackgroundColor(Color.GREEN);
+        }
+        else{
+            mainView.setBackgroundColor(Color.YELLOW);
+        }
     }
 }
